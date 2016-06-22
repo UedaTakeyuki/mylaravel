d85e58dd78375b854be1f9abdd2ba03778d15ae5
Dr. Takeyuki Ueda <gde00107@nifty.com>
2016-06-22 11:37:00 +0000

    ララ帳 04

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,7 +10,26 @@ class PagesController extends Controller
 {
     //
     public function about() {
+        // 配列に値をセット
+        /*
+        $data = [];
+        $data["first_name"] = "Luke";
+        $data["last_name"] = "Skywalker";
+
+        // view関数の第２引数に配列を渡す
+        return view('pages.about', $data);        
+        */
+        // ↓上と同じ
+        // 変数に値をセット
+        $first_name = "Luke";
+        $last_name = "Skywalker";
+ 
+        // view関数の第２引数に compact関数を使う
+        return view('pages.about', compact('first_name', 'last_name'));        
+       
+        /* 以前のコード
         return view('pages.about');
+        */
     }
     public function contact() {
         return view('pages.contact');
