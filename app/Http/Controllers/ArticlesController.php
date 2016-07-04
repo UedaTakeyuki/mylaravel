b5c11afa08fe8740b3cb4b3ae80854d740ae360a
Dr. Takeyuki Ueda <gde00107@nifty.com>
2016-07-04 01:12:53 +0000

    ララ帳(18)

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
 use App\Article;
+use App\Http\Requests\ArticleRequest;  // ①'
 
 class ArticlesController extends Controller
 {
@@ -29,15 +28,15 @@ class ArticlesController extends Controller
         return view('articles.create');
     }
     
-    public function store(Request $request) {
-        // ①フォームの入力値を取得
+    public function store(ArticleRequest $request) {
+/*        // ①フォームの入力値を取得
         $rules = [    // ②
             'title' => 'required|min:3',
             'body' => 'required',
             'published_at' => 'required|date',
         ];
         $this->validate($request, $rules);  // ③ 
-        
+*/        
         // ②マスアサインメントを使って、記事をDBに作成
         Article::create($request->all());
  
